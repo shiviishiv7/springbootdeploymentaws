19a044845b734a2365e17bdf4645f8a3dee839ac
shiviishiv7 <shiviishiv7@gmail.com>
2023-05-19 19:35:25 +0530

    first commit

---
+++ src/main/java/com/example/springdeploy/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.springdeploy.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello-world")
+public class HelloController {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello wolrd";
+    }
+}
